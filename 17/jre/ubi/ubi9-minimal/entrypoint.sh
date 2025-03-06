dc3549e49ab32626647273bc83a0474d6cfa5e47
Bodo Graumann <mail@bodograumann.de>
2025-03-06 15:29:33 +0100

    Regenerate all entrypoint scripts
    DCO 1.1 Signed-off-by: Bodo Graumann

--- 17/jre/ubi/ubi9-minimal/entrypoint.sh
+++ 17/jre/ubi/ubi9-minimal/entrypoint.sh
@@ -69,13 +69,18 @@ if [ -n "$USE_SYSTEM_CA_CERTS" ]; then
 
         # We might have multiple certificates in the file. Split this file into single files. The reason is that
         # `keytool` does not accept multi-certificate files
-        csplit -s -z -b %02d.crt -f "$tmp_dir/$BASENAME-" "$i" '/-----BEGIN CERTIFICATE-----/' '{*}'
+        awk '
+            BEGIN { file = "/dev/null" }
+            /^-----BEGIN CERTIFICATE-----$/ { file = sprintf("'"$tmp_dir/$BASENAME-"'%02.crt", idx) }
+            { print $0 >> file }
+            /^-----END CERTIFICATE-----$/ { file = "/dev/null"; idx++ }
+        ' < "$i"
 
         for crt in "$tmp_dir/$BASENAME"-*; do
             # Extract the Common Name (CN) and Serial Number from the certificate
             CN=$(openssl x509 -in "$crt" -noout -subject -nameopt -space_eq | sed -n 's/^.*CN=\([^,]*\).*$/\1/p')
             SERIAL=$(openssl x509 -in "$crt" -noout -serial | sed -n 's/^serial=\(.*\)$/\1/p')
-            
+
             # Check if an alias with the CN already exists in the keystore
             ALIAS=$CN
             if keytool -list -keystore "$JRE_CACERTS_PATH" -storepass changeit -alias "$ALIAS" >/dev/null 2>&1; then
